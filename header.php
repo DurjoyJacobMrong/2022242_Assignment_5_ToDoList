cd3b958733858e117efc49abc735aac3e3dfde7f
DurjoyJacobMrong <114507204+DurjoyJacobMrong@users.noreply.github.com>
2023-08-30 05:00:42 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Todo list</title>
+    <style>
+    body{
+    padding:100px;
+    margin:0;
+    background: rgb(11,11,34);
+    background:linear-gradient(90deg, rgba(11,11,34,1) 43%, rgba(2,110,21,1) 100%);
+    background-size:cover;
+ 
+    
+    }
+    </style>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  </head>
+  <body>
